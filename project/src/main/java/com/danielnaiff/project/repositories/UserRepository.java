3af7b0babcb31183eb29583db0764cd4dce4d51f
Daniel Naiff <danielnaiff344@gmail.com>
2025-03-02 19:20:20 -0300

    Service layer, component registration

--- project/src/main/java/com/danielnaiff/project/repositories/UserRepository.java
+++ project/src/main/java/com/danielnaiff/project/repositories/UserRepository.java
@@ -2,7 +2,9 @@ package com.danielnaiff.project.repositories;
 
 import com.danielnaiff.project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 //classe que salva os dados
 }
